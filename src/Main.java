520b6613f76e726de80a861f7dfd4127d0c81df2
Sena7Aslantas <102954799+Sena7Aslantas@users.noreply.github.com>
2023-07-25 00:47:54 +0300

    harfyapma

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        String[][] letter = new String[5][3];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (j==0) {
+                    letter[i][j] = " * ";
+                } else if (i%2==0 &&j==1 ) {
+                    letter[i][j] = " * ";
+
+                }
+                else if((i==1 ||i==3 )&&j==2){
+                    letter[i][j] = " * ";
+                }
+                else {
+                    letter[i][j] = " ";
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+    }
+}
